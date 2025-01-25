63a7782388ba3d1918dc6866a8613c6aecad8f09
Toby Reid <toby.reid@usu.edu>
2025-01-24 20:29:54 -0700

    testing physics

--- test/test_stock_man_body.gd
+++ test/test_stock_man_body.gd
@@ -16,7 +16,7 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("left", "right")
+	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
