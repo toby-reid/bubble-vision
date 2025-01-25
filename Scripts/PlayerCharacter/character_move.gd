a916cd95c29362d5600f0ee555754689e8d3fd5c
Toby Reid <toby.reid@usu.edu>
2025-01-25 12:42:05 -0700

    added patient animations

---
+++ Scripts/PlayerCharacter/character_move.gd
@@ -0,0 +1,29 @@
+extends CharacterBody2D
+
+
+const SPEED = 100.0
+const JUMP_VELOCITY = -200.0
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if is_on_floor():
+		if Input.is_action_just_pressed("jump"):
+			velocity.y = JUMP_VELOCITY
+	elif Input.is_action_pressed("jump") and velocity.y >= 0:
+		velocity.y -= get_gravity().y * delta / 2
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x = direction * SPEED
+		$AnimatedSprite2D.flip_h = direction > 0
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
