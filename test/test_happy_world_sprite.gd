455a0a1dc5cd2ad2ada26e756b63053c1d62c49c
Toby Reid <toby.reid@usu.edu>
2025-01-24 23:38:27 -0700

    got the mask to visible status!

--- test/test_happy_world_sprite.gd
+++ test/test_happy_world_sprite.gd
@@ -6,10 +6,11 @@ extends Sprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	mask_viewport = $"../../SubViewport"
+	material = get_parent().material
 
 func _process(delta):
 	var mask_texture = mask_viewport.get_texture()
-	material.set_shader_param("mask_texture", mask_texture)
-	material.set_shader_param("mask_offset", mask_position / mask_scale)
-	material.set_shader_param("mask_scale", mask_scale)
+	material.set_shader_parameter("mask_texture", mask_texture)
+	material.set_shader_parameter("mask_offset", mask_position / mask_scale)
+	material.set_shader_parameter("mask_scale", mask_scale)
