e805e36dfde0ad996389ed3880affc7c9aadfb75
hyrum <hyrum.d.smith@gmail.com>
2025-01-24 20:58:19 -0700

    Start and Quit buttons work

---
+++ Scenes/menu.gd
@@ -0,0 +1,23 @@
+extends VBoxContainer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_start_pressed():
+	get_tree().change_scene_to_file("res://test/test_scene.tscn")
+
+
+func _on_options_pressed():
+	pass # Replace with function body.
+
+
+func _on_quit_pressed():
+	get_tree().quit()
