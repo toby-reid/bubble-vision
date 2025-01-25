52b918a6342d1b2d76597b1af0eadbd64cf792b2
Toby Reid <toby.reid@usu.edu>
2025-01-25 16:47:39 -0700

    ready for demo!

--- Scripts/PlayerCharacter/character_move.gd
+++ Scripts/PlayerCharacter/character_move.gd
@@ -2,6 +2,10 @@ extends CharacterBody2D
 
 const SPEED = 100.0
 const JUMP_VELOCITY = -200.0
+var sprite: AnimatedSprite2D
+
+func _ready() -> void:
+	sprite = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -12,16 +16,28 @@ func _physics_process(delta: float) -> void:
 	if is_on_floor():
 		if Input.is_action_just_pressed("jump"):
 			velocity.y = JUMP_VELOCITY
-	elif Input.is_action_pressed("jump") and velocity.y >= 0:
-		velocity.y -= get_gravity().y * delta / 2
+	else:
+		if velocity.y < 0:
+			sprite.play("jumping")
+		else:
+			sprite.play("falling")
+			if Input.is_action_pressed("jump"):
+				velocity.y -= get_gravity().y * delta / 2
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
-		$AnimatedSprite2D.flip_h = direction > 0
+		sprite.flip_h = direction > 0
+		if is_on_floor():
+			sprite.play("walking")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if is_on_floor():
+			if Input.is_action_pressed("duck"):
+				sprite.play("ducking")
+			else:
+				sprite.play("idle")
 
 	move_and_slide()
--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -14,7 +14,7 @@ func _process(delta):
 
 
 func _on_start_pressed():
-	get_tree().change_scene_to_file("res://test/test_scene.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Levels/Cell.tscn")
 
 
 func _on_options_pressed():
